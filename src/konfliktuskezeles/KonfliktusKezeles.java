8b57d4a1a5fb5565e05aee70be51982184fcf111
kelekevin23 <kelekevin11@gmail.com>
2022-03-09 11:48:03 +0100

    1. név inicializálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,7 +3,7 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "Roli";
         String nev2;
     }
 
