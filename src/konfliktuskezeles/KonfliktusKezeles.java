0cbf5c338dc8d86a26b7b2b6721aef0873e5c56c
kelekevin23 <78788931+kelekevin23@users.noreply.github.com>
2022-03-09 11:57:04 +0100

    int deklarálás

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,6 +3,7 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i = 23;
         String nev1 = "Roli";
         String nev2 = "Kevin";
         String nev3;
