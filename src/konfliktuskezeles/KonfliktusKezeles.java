2c140f1256032adb41b22d7033b81ac97fa258b1
kelekevin23 <78788931+kelekevin23@users.noreply.github.com>
2022-03-09 11:55:29 +0100

    3. név deklarálása

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,8 +3,10 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i = 23;
         String nev1 = "Roli";
         String nev2 = "Kevin";
+        int nev3;
     }
 
 }
